14b6bbf962a2d678ac33a80047d6aff0b0103e01
bion-slmn <bionsol25@gmail.com>
2026-09-07 23:26:30 +0300

    updated to add  a comma in cors

--- matatu-sacco-backend/src/main.ts
+++ matatu-sacco-backend/src/main.ts
@@ -39,7 +39,7 @@ async function bootstrap() {
     origin: [
       'https://long-distance-shuttle-eek9.vercel.app',
       'http://localhost:3000', // your local frontend dev
-      'http://localhost:5173'
+      'http://localhost:5173',
       'https://shuttlehub.co.ke'
     ],
     credentials: true,
